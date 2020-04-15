c177f80303bee26a401055afc982cf981744bbcf
Nicolò Cucinotta <iamonedevarmy@gmail.com>
2020-04-15 12:38:43 +0200

    Creato menu principale con controllo su quit

--- src/main/java/it/uniba/main/AppMain.java
+++ src/main/java/it/uniba/main/AppMain.java
@@ -2,8 +2,11 @@ package it.uniba.main;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.PrintStream;
+import java.io.UnsupportedEncodingException;
 import java.net.URISyntaxException;
 import java.security.GeneralSecurityException;
+import java.util.Scanner;
 
 import it.uniba.sotorrent.GoogleDocsUtils;
 
@@ -27,30 +30,34 @@ public final class AppMain {
 	 *
 	 * @param args The command-line arguments.
 	 */
-	public static void main(final String[] args) {
+	public static void main(final String[] args) throws UnsupportedEncodingException {
+		System.out.println("Scrivi help per la lista dei comandi, play per iniziare una nuova partita. Qualunque notazione algebrica prima di play non sarà accettata");
+		Scanner sc = new Scanner(System.in);
+		System.setOut(new PrintStream(System.out, false, "UTF-8"));
+		menu(sc);
 
-		System.out.println("Current working dir: " + System.getProperty("user.dir"));
-
-		if (args.length > 0) {
-			switch (args[0]) {
-			case "it":
-				System.out.println("Applicazione avviata.");
-				break;
-
-			case "en":
-				System.out.println("Application started.");
-				break;
-
-			default:
-				System.out.println("Specify the language. "
-								   + "Languages supported: 'it' or 'en'");
-				break;
-			}
-		} else {
-			System.out.println("Using default language 'en'");
-			System.out.println("Application started.");
+	}
+	
+	private static void menu(Scanner sc) {
+		String command;
+		do {
+			command = sc.nextLine();
+			/*try {
+			Parser.parseCommand(command.toLowerCase());
+			} catch(IllegalMoveException ex) {
+				System.out.println(ex.getMessage());
+			}*/
+			
+		} while (!command.equals("quit"));
+		System.out.println("Sei sicuro di voler uscire? S/N (default: N)");
+		String confirmation = sc.nextLine().toLowerCase();
+		if (confirmation.equals("s")) {
+			sc.close();
+			System.exit(0);
 		}
-
+		System.out.println("Continuo la partita:");
+		menu(sc);
+		return;		
 	}
 
 }
